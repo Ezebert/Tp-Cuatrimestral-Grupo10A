023a01274ad879fc5afc5a689789cb5111910bf3
Leonardo Forte <leonardojforte@gmail.com>
2024-10-19 17:27:27 -0300

    Se agrega Dominio completo y scriptSQL

---
+++ Dominio/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Productos
+    {
+        public int ProductoId {  get; set; }
+        public string? Nobmre {  get; set; }
+        public string? Descripcion {  get; set; }
+        public float Precio {  get; set; }
+        public int stock {  get; set; }
+        public int CategoriaId {  get; set; }
+        public bool Estado { get; set; }
+    }
+}
